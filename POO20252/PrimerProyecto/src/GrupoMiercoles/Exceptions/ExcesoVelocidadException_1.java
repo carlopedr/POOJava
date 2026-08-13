a2c834ea4238bf9d79096dab3eb6446c35f688d3
Carlos Pedraza <carlospedraza@usco.edu.co>
2026-08-13 15:18:50 -0500

    XXX

---
+++ POO20252/PrimerProyecto/src/GrupoMiercoles/Exceptions/ExcesoVelocidadException_1.java
@@ -0,0 +1,10 @@
+package GrupoMiercoles.Exceptions;
+
+import GrupoMartes.Exceptions.*;
+import Ex.*;
+
+public class ExcesoVelocidadException_1 extends Exception {
+    public ExcesoVelocidadException_1() {
+        super("¡Cuidado, error! Velocidad máxima alcanzada!");
+    }
+}
